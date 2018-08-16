0634471f39c7ca60d5680d975d67ef4abbf2052f
hungrated <zhang295415658@qq.com>
2018-08-17 00:08:30 +0800

    master: modify functions

--- src/qzone/browse.py
+++ src/qzone/browse.py
@@ -27,17 +27,51 @@ def browse_qzone():
     ps.send_keys(decrypt_str(USER_PWD))
 
     print('click the login button...')
-    btn = browser.find_element_by_id('login_button')
+    btn1 = browser.find_element_by_id('login_button')
     time.sleep(1)
-    btn.click()
+    btn1.click()
 
     print('navigate to user home page...')
     time.sleep(1)
     browser.get('https://user.qzone.qq.com/{}'.format(decrypt_str(USER_NAME)))
 
+    print('navigate to moment page...')
+    time.sleep(1)
+    btn2 = browser.find_element_by_xpath(".//*[@title='说说']")
+    btn2.click()
+
+    print('loading full page...')
+    time.sleep(1)
+    # browser.execute_script(
+    #     """
+    #     (function () {
+    #       var y = 0;
+    #       var step = 100;
+    #       window.scroll(0, 0);
+    #
+    #       function f() {
+    #         if (y < (document.body.scrollHeight)/5) {
+    #           y += step;
+    #           window.scroll(0, y);
+    #           setTimeout(f, 100);
+    #         } else {
+    #           window.scroll(0, 0);   //滑动到顶部
+    #           document.title += "scroll-done";
+    #         }
+    #       }
+    #       setTimeout(f, 1000);
+    #     })();
+    #     """
+    # )
+
     print('fetching page source...')
+    time.sleep(3)
+
+    frame = browser.find_element_by_xpath('//*[@id="app_container"]/iframe')
+    browser.switch_to.frame(frame)
     html = browser.page_source
     print(html)
 
-    print('quit browser...')
-    browser.quit()
+    time.sleep(99999)
+    # print('quit browser...')
+    # browser.quit()
